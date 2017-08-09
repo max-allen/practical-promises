25b44cbee7209bd98d1ec206468a3e5f17b2fcb5
MaxAllen <maxton.allen@gmail.com>
2017-08-09 13:02:49 -0400

    chapter 1 completed

--- src/pledge.js
+++ src/pledge.js
@@ -3,11 +3,38 @@
 Promises Workshop: build the pledge.js ES6-style promise library
 ----------------------------------------------------------------*/
 // YOUR CODE HERE:
+function $Promise(executor){
+	if(typeof executor !== 'function'){
+		throw TypeError('The executor function was not a function!');
+	}
 
+	this._state = 'pending';
+	this._value;
+	
+	const resolve = function(data){
+		this._internalResolve(data)
 
+	}
+	const reject = function(data){
+		this._internalReject(data)
 
+	}
+	executor(resolve.bind(this),reject.bind(this)); //bind or arrow functions
+}
 
+$Promise.prototype._internalResolve = function (data) {
+	if(this._state === 'pending'){
+		this._value = data
+		this._state = 'fulfilled'
+	}
+}
 
+$Promise.prototype._internalReject = function (reason) {
+	if(this._state === 'pending'){
+		this._value = reason;
+		this._state = 'rejected'
+	}
+}
 
 /*-------------------------------------------------------
 The spec was designed to work with Test'Em, so we don't
